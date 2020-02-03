ff724f0558a4fea6a99ee54ebd6fb84a5fc378c0
ticochuck <chucklivilla@gmail.com>
2020-02-02 22:38:52 -0800

    added total per hour line

--- js/apps.js
+++ js/apps.js
@@ -1,39 +1,41 @@
 'use strict';
 var hoursOperation = [' 6am',' 7am',' 8am',' 9am','10am','11am','12pm',' 1pm',' 2pm',' 3pm',' 4pm',' 5pm',' 6pm',' 7pm'];
 var allStores = [];
-
+var totalsPerHour = [];
 
 //this is my Object Constructor 
-function Store(Location, minCustPerHour, maxCustPerHour, avgCookiePerCust, soldCookiesPerHour, totalCookies) {
+function Store(Location, minCustPerHour, maxCustPerHour, avgCookiePerCust, [], totalCookies) {
   this.Location = Location;
   this. minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
   this.avgCookiePerCust = avgCookiePerCust;
-  this.soldCookiesPerHour = soldCookiesPerHour;
+  this.soldCookiesPerHour = [];
   this.totalCookies = totalCookies;
   allStores.push(this);
 };
 
-var seattle = new Store('Seattle', 23, 65, 6.3, [], 0);
-var tokyo = new Store('Tokyo', 3, 24, 1.2, [], 0);
-var dubai = new Store('Dubai', 11, 38, 3.7, [], 0);
-var paris = new Store('Paris', 20, 37, 2.3, [], 0);
-var lima = new Store('Lima', 2, 16, 4.6, [], 0);
+//create Stores for different locations
+new Store('Seattle', 23, 65, 6.3, [], 0);
+new Store('Tokyo', 3, 24, 1.2, [], 0);
+new Store('Dubai', 11, 38, 3.7, [], 0);
+new Store('Paris', 20, 38, 2.3, [], 0);
+new Store('Lima', 2, 16, 4.6, [], 0);
 
+//CALCULATIONS SECTION
 //generate random customers per hour
 Store.prototype.custPerHour = function() {
   return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour; 
 };
 
-//generate total cookies sold per hour
+//calculates total cookies sold per hour and daily totals 
 Store.prototype.cookiesPerHour = function() {
-  for (var i = 0; i < hoursOperation.length; i++) {
-    this.soldCookiesPerHour[i] = Math.round(this.soldCookiesPerHour[i] = this.avgCookiePerCust * this.custPerHour());
-    this.totalCookies += this.soldCookiesPerHour[i];
+  for (var j = 0; j < hoursOperation.length; j++) {
+    this.soldCookiesPerHour[j] = Math.round(this.soldCookiesPerHour[j] = this.avgCookiePerCust * this.custPerHour());
+    this.totalCookies += this.soldCookiesPerHour[j];
   }
-  return this.soldCookiesPerHour;
 };
 
+//RENDERING SECTION
 //renderInfo function is used to pass info to the sales.html page using 2 parameters: create.element and text.content
 var renderInfo = function(tableElement, textContent) {
   var tableMain = document.getElementById('sales');
@@ -42,26 +44,44 @@ var renderInfo = function(tableElement, textContent) {
   tableMain.appendChild(tableData);  
 };
 
-//Render cookies per hour per Location
+//render table header and footer
+renderInfo('th') //appends table header 
+for (var iHeaders = 0; iHeaders < hoursOperation.length; iHeaders++) {
+  renderInfo('th', hoursOperation[iHeaders]);
+}
+renderInfo('th', 'Daily Location Total'); //appends header for daily totals
+
+//Render cookies per hour per Location and daily totals
 Store.prototype.rederData = function() {
   renderInfo('tr');
   for (var i = 0; i < hoursOperation.length; i++) {
     if (i === 0) {
-      renderInfo('td', this.Location)
+      renderInfo('td', this.Location); //appends location name to table
     }
-    renderInfo('td', this.cookiesPerHour()[i])
+    renderInfo('td', this.soldCookiesPerHour[i]); //appends total cookies sold per hour
   }
+  renderInfo('td', this.totalCookies); //appends total daily cookies
 };
 
-//render table headers
-renderInfo('th')
-for (var iHeaders = 0; iHeaders < hoursOperation.length; iHeaders++) {
-  renderInfo('th', hoursOperation[iHeaders]);
-}
 
 //render info calculated in the prototypes
 for (var renderAllStores = 0; renderAllStores < allStores.length; renderAllStores++) {
-  allStores[renderAllStores].rederData();
   allStores[renderAllStores].cookiesPerHour();
-};
+  allStores[renderAllStores].rederData();
+}
+
+// console.log(totalsPerHour)
+var hourlyTotals = function () { 
+  renderInfo('tr');
+  renderInfo('td', 'Totals'); //appends footer for hourly totals  
+  for (var a = 0; a < hoursOperation.length; a++) {
+    var pleaseWork = 0;  
+    for (var b = 0; b < allStores.length; b++) {
+      totalsPerHour[a] = allStores[b].soldCookiesPerHour[a]; 
+      pleaseWork += totalsPerHour[a];
+    } 
+    renderInfo('td', pleaseWork);
+  }
+}
+hourlyTotals();
 
